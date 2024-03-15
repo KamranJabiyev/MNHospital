6e4953df94138d4fa69bdc43c4de5f2ecdb2c46c
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-15 10:32:19 +0400

    first commit

---
+++ src/Core/MNHospital.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace MNHospital.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ src/Presentation/MNHospital.API/Controllers/DepartamentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MNHospital.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentsController : ControllerBase
+    {
+        // GET: api/<DepartamentsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<DepartamentsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DepartamentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DepartamentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DepartamentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
