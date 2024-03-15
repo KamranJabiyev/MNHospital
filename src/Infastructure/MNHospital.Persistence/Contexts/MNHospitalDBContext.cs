36bdd8d7d846d001db9b94ad8eb29029857185f2
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-15 11:51:48 +0400

    MH-5 Added Context class

---
+++ src/Infastructure/MNHospital.Persistence/Contexts/MNHospitalDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MNHospital.Domain.Entities;
+using MNHospital.Persistence.Configurations;
+
+namespace MNHospital.Persistence.Contexts;
+
+public class MNHospitalDBContext:DbContext
+{
+    public MNHospitalDBContext(DbContextOptions<MNHospitalDBContext> options):base(options)
+    {
+    }
+
+    public DbSet<Department> Departments { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { 
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DepartmentConfiguration).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
