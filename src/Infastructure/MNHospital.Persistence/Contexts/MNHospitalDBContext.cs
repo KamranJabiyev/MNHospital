47d622bb1b079d5b07cb0abeb60510f3b4a3f8ba
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-15 12:13:05 +0400

    MH-5 Added Migration class

--- src/Infastructure/MNHospital.Persistence/Contexts/MNHospitalDBContext.cs
+++ src/Infastructure/MNHospital.Persistence/Contexts/MNHospitalDBContext.cs
@@ -18,4 +18,5 @@ public class MNHospitalDBContext:DbContext
         base.OnModelCreating(modelBuilder);
     }
 
+
 }
--- src/Presentation/MNHospital.API/Program.cs
+++ src/Presentation/MNHospital.API/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using MNHospital.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<MNHospitalDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
