e50b6a1b356b6211585733f8d4905bf3da7ea5da
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-15 11:31:50 +0400

    MH-7 Added department fluent configuration

---
+++ src/Infastructure/MNHospital.Persistence/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MNHospital.Domain.Entities;
+
+namespace MNHospital.Persistence.Configurations;
+
+public class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.Property(d => d.Name)
+            .IsRequired()
+            .HasMaxLength(255);
+    }
+}
