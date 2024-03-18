e5b7a4d01c45fd6166b53904586c3cf566b75bc4
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-18 11:40:38 +0400

    MH-9 Added Read and Write repository classes

---
+++ src/Infastructure/MNHospital.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MNHospital.Application.Abstraction.Repositories;
+using MNHospital.Domain.Entities;
+using MNHospital.Persistence.Contexts;
+using System.Linq.Expressions;
+
+namespace MNHospital.Persistence.Repositories;
+
+public class ReadRepository<T>(MNHospitalDBContext context) : IReadRepository<T> where T : class, IEntity, new()
+{
+    public DbSet<T> Table => context.Set<T>();
+
+    public IQueryable<T> GetAll(bool isTracking = false, params string[] includes)
+    {
+        IQueryable<T> query = Table.AsQueryable();
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+        return isTracking ? query : query.AsNoTracking();
+    }
+
+    public IQueryable<T> GetAllExpression(int take, int skip, Expression<Func<T, bool>>? expression = null, bool isTracking = false, params string[] includes)
+    {
+        IQueryable<T> query = Table.AsQueryable();
+        if (expression is not null)
+        {
+            query = query.Where(expression);
+        }
+
+        query = query.Skip(skip).Take(take);
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return isTracking ? query : query.AsNoTracking();
+    }
+
+    public IQueryable<T> GetAllExpressionOrderBy(int take, int skip, Expression<Func<T, object>> orderByExpression, bool isOrderByAsc = true, Expression<Func<T, bool>>? expression = null, bool isTracking = false, params string[] includes)
+    {
+        IQueryable<T> query = Table.AsQueryable();
+        if (expression is not null)
+        {
+            query = query.Where(expression);
+        }
+
+        query = isOrderByAsc ? query.OrderBy(orderByExpression) : query.OrderByDescending(orderByExpression);
+        query = query.Skip(skip).Take(take);
+        foreach (var include in includes)
+        {
+            query = query.Include(include);
+        }
+
+        return isTracking ? query : query.AsNoTracking();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await Table.FindAsync(id);
+    }
+
+    public Task<T?> GetBySingleExpressionAsync(Expression<Func<T, bool>> expression, bool isTracking = false)
+    {
+        IQueryable<T> query = Table.Where(expression);
+        query = isTracking ? query : query.AsNoTracking();
+        var response = query.FirstOrDefaultAsync();
+        return response;
+    }
+}
---
+++ src/Infastructure/MNHospital.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MNHospital.Application.Abstraction.Repositories;
+using MNHospital.Domain.Entities;
+using MNHospital.Persistence.Contexts;
+
+namespace MNHospital.Persistence.Repositories;
+
+public class WriteRepository<T>(MNHospitalDBContext context) : IWriteRepository<T> where T : class, IEntity, new()
+{
+    public DbSet<T> Table => context.Set<T>();
+
+    public async Task AddAsync(T entity)
+    {
+        await Table.AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(ICollection<T> entities)
+    {
+        await Table.AddRangeAsync(entities);
+    }
+
+    public void Remove(T entity)
+    {
+        Table.Remove(entity);
+    }
+
+    public void RemoveRange(ICollection<T> entities)
+    {
+        Table.RemoveRange(entities);
+    }
+
+    public void Update(T entity)
+    {
+        Table.Update(entity);
+        //context.Entry<T>(entity).State = EntityState.Modified;
+    }
+
+    public async Task SaveChangeAsync()
+    {
+        await context.SaveChangesAsync();
+    }
+}
