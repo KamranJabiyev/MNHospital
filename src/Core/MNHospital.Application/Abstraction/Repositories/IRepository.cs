04c873b240f21e4bf83f7d36a02a4aff85035a16
KamranJabiyev <kamran.jabiyev@code.edu.az>
2024-03-18 09:52:21 +0400

    MH-8 added repository interfaces

---
+++ src/Core/MNHospital.Application/Abstraction/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MNHospital.Domain.Entities;
+
+namespace MNHospital.Application.Abstraction.Repositories;
+
+public interface IRepository<T> where T : class,IEntity,new()
+{
+    public DbSet<T> Table { get; }
+}
---
+++ src/Core/MNHospital.Application/Abstraction/Repositories/IWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using MNHospital.Domain.Entities;
+
+namespace MNHospital.Application.Abstraction.Repositories;
+
+public interface IWriteRepository<T>:IRepository<T> where T : class,IEntity,new()
+{
+    Task AddAsync(T entity);
+    Task AddRangeAsync(ICollection<T> entities);
+    void Remove(T entity);
+    void RemoveRange(ICollection<T> entities);
+    void Update(T entity);
+    Task SaveChangeAsync();
+}
